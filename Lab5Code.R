9c7f5530d2371f781486512bcc422091fc3147ba
Charlie Hooey <charleshooey@gmail.com>
2025-02-22 17:19:22 -0500

    Function created

--- Lab5Code.R
+++ Lab5Code.R
@@ -2,13 +2,13 @@ library(tidyverse)
 
 allentown_data <- read_csv("data/essentia.data.allentown.csv")
 song_data <- read_csv("data/essentia.data.csv") 
-
+song_stats <- function(feature){
 song_data|>
-  group_by(artist) |>
-  summarize( Min = min(overall_loudness, na.rm = TRUE),
-             Q1 = quantile(overall_loudness, 0.25, na.rm = TRUE),
-             Q3 = quantile(overall_loudness, 0.75, na.rm = TRUE),
-             Max = max(overall_loudness, na.rm = TRUE)) |>
+group_by(artist)|>
+  summarize( Min = min(get(feature), na.rm = TRUE),
+             Q1 = quantile(get(feature), 0.25, na.rm = TRUE),
+             Q3 = quantile(get(feature), 0.75, na.rm = TRUE),
+             Max = max(get(feature), na.rm = TRUE)) |>
   mutate( 
     IQR = Q3 - Q1,
     Lower_Fence = Q1 - 1.5 * IQR,
@@ -18,7 +18,7 @@ song_data|>
     out.of.range = if_else((allentown_data$overall_loudness < Min | allentown_data$overall_loudness > Max), 
                           TRUE, FALSE),
     unusual = if_else((allentown_data$overall_loudness < Lower_Fence | allentown_data$overall_loudness > Upper_Fence), 
-                     TRUE, FALSE)
+                     TRUE, FALSE),
     description = case_when(
       out.of.range ~ "Out of Range",
       unusual ~ "Outlying",
@@ -26,6 +26,7 @@ song_data|>
 
   )
   )
+}
 
-
+song_stats("spectral_energy")
   
